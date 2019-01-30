59206016957d2b286edbef770fd0ecdbd937ae26
Torsten Schmits <torstenschmits@gmail.com>
2019-01-30 01:22:15 +0100

    error logging helpers

--- lib/Myo/Log.hs
+++ lib/Myo/Log.hs
@@ -1,8 +1,10 @@
 module Myo.Log(
   debug,
   info,
+  err,
   debugS,
   infoS,
+  errS,
   R.p,
   R.prefixed,
   trees,
@@ -14,7 +16,7 @@ import Data.Foldable (traverse_)
 import Chiasma.Ui.ShowTree (printViewTree)
 import Ribosome.Control.Ribo (Ribo)
 import qualified Ribosome.Control.Ribo as Ribo (inspect)
-import qualified Ribosome.Log as R (debug, info, p, prefixed)
+import qualified Ribosome.Log as R (debug, info, err, p, prefixed)
 import Myo.Data.Myo (Myo)
 import Myo.Ui.View (envTreesLens)
 
@@ -24,11 +26,17 @@ debug = R.debug "myo"
 info :: String -> Ribo e ()
 info = R.info "myo"
 
+err :: String -> Ribo e ()
+err = R.err "myo"
+
 debugS :: Show a => a -> Ribo e ()
 debugS = R.debug "myo"
 
 infoS :: Show a => a -> Ribo e ()
 infoS = R.info "myo"
+
+errS :: Show a => a -> Ribo e ()
+errS = R.err "myo"
 
 trees :: Myo ()
 trees = do
