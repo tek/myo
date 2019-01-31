0e97d4631fff2a71b317e75bd0f9fd1e5d2a0a02
Torsten Schmits <torstenschmits@gmail.com>
2019-01-31 22:57:01 +0100

    missing error report pattern match

--- lib/Myo/Command/Data/RunError.hs
+++ lib/Myo/Command/Data/RunError.hs
@@ -2,11 +2,13 @@ module Myo.Command.Data.RunError(
   RunError(..),
 ) where
 
-import System.Log (Priority(ERROR))
+import Chiasma.Data.Ident (identString)
 import Ribosome.Error.Report (ReportError(..), ErrorReport(ErrorReport))
+import System.Log (Priority(ERROR, NOTICE))
 
+import qualified Myo.Command.Data.Command as Cmd (Command(Command))
 import Myo.Command.Data.CommandError (CommandError(..))
-import Myo.Command.Data.RunTask (RunTask)
+import Myo.Command.Data.RunTask (RunTask(RunTask))
 
 data RunError =
   RunError String
@@ -20,3 +22,7 @@ instance ReportError RunError where
   errorReport (RunError e) =
     ErrorReport e ["running command failed:", e] ERROR
   errorReport (Command e) = errorReport e
+  errorReport (NoRunner task@(RunTask (Cmd.Command _ ident _ _) _ _)) =
+    ErrorReport user ["no runner for task:", show task] NOTICE
+    where
+      user = "no runner available for command `" ++ identString ident ++ "`"
