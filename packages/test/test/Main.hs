dfbd7090cf37193a149522febf7ae693b20f6237
Torsten Schmits <git@tryp.io>
2023-10-14 23:00:38 +0200

    add test to Main

--- packages/test/test/Main.hs
+++ packages/test/test/Main.hs
@@ -4,7 +4,7 @@ import Myo.Test.Command.CommandMenuTest (test_commandMenu)
 import Myo.Test.Command.CommandSpecTest (test_commandSpec)
 import Myo.Test.Command.HistoryMenuTest (test_historyMenu, test_historyMenuDelete, test_historyMenuEdit)
 import Myo.Test.Command.HistoryTest (test_history)
-import Myo.Test.Command.TestTest (test_testCommand, test_testCommandDefault)
+import Myo.Test.Command.TestTest (test_testCommand, test_testCommandDefault, test_testCommandEmpty)
 import Myo.Test.Command.UpdateTest (test_updateCommands)
 import Myo.Test.Command.VimTestTest (test_vimTest)
 import Myo.Test.CompleteTest (test_completeCommand)
@@ -62,7 +62,8 @@ tests =
         unitTest "command with parameters" test_runParamCommand,
         unitTest "test command with optparse args" test_runParamCommandOptparse,
         unitTest "test command" test_testCommand,
-        unitTest "test command with custom command with defaults" test_testCommandDefault
+        unitTest "test command with custom command with defaults" test_testCommandDefault,
+        unitTest "test command with empty cmdline" test_testCommandEmpty
       ],
       test_commandSpec
     ],
